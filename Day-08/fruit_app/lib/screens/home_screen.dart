2ffdd006a5b26c46bb3c592e48eefefa4334ed5d
UdeepaSandakal <2017asp02@vau.jfn.ac.lk>
2023-05-20 10:22:01 +0530

    home screen begin

--- Day-08/fruit_app/lib/screens/home_screen.dart
+++ Day-08/fruit_app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_app/utils/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,9 +11,49 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("HomeScreen"),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 30,
+                    backgroundColor: kSecondaryColor,
+                    backgroundImage: AssetImage('assets/user.png'),
+                  ),
+                  const SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w300,
+                          fontSize: 15,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'Udeepa Sandakal',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  // icon widget
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
