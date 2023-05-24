8ad8cfd71f614e6e78e63b151aea7c63e8ed8f3d
UdeepaSandakal <2017asp02@vau.jfn.ac.lk>
2023-05-24 15:30:31 +0530

    routing details screen

---
+++ Day-08/fruit_app/lib/screens/details_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:fruit_app/data/data.dart';
+
+class DetailsScreen extends StatefulWidget {
+  const DetailsScreen({super.key, required Product product});
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
