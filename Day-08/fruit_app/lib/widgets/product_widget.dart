e03de0d21fde23cd04d814cd66d05b1fa736d7c7
UdeepaSandakal <2017asp02@vau.jfn.ac.lk>
2023-05-24 14:44:30 +0530

    product details

--- Day-08/fruit_app/lib/widgets/product_widget.dart
+++ Day-08/fruit_app/lib/widgets/product_widget.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:fruit_app/utils/colors.dart';
+import 'package:fruit_app/utils/utilis.dart';
 import '../data/data.dart';
 
 class ProductWidget extends StatelessWidget {
@@ -21,6 +24,49 @@ class ProductWidget extends StatelessWidget {
               child: Image.asset(
                 product.image,
                 height: 80,
+              ),
+            ),
+          ),
+
+          //heart icon
+          Positioned(
+            right: 0,
+            top: 0,
+            child: Container(
+              height: 30,
+              width: 30,
+              margin: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: boxShadow,
+              ),
+              child: Icon(
+                FontAwesomeIcons.solidHeart,
+                color: product.isSelected ? kPrimaryColor : Colors.black87,
+                size: 15,
+              ),
+            ),
+          ),
+
+          //product details
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Container(
+              height: 100,
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: product.color.withOpacity(0.5),
+                    blurRadius: 2,
+                    spreadRadius: 2,
+                    offset: const Offset(0, 1),
+                  ),
+                ],
               ),
             ),
           ),
