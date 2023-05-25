d18ede1273beb59206dfd0e6ad9ff1a435bbd3f4
UdeepaSandakal <2017asp02@vau.jfn.ac.lk>
2023-05-25 13:55:54 +0530

    nutrient screen icons

--- Day-08/fruit_app/lib/widgets/nutrient_widget.dart
+++ Day-08/fruit_app/lib/widgets/nutrient_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:fruit_app/data/data.dart';
 import 'package:fruit_app/utils/utilis.dart';
 
@@ -41,7 +42,60 @@ class NutrientWidget extends StatelessWidget {
               ),
             ],
           ),
+          const SizedBox(height: 5),
+
+          //bulidig the indicators
+          Row(
+            children: buildIndicators(
+              product.nutrients[index][0],
+              int.parse(
+                product.nutrients[index][1],
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  List<Widget> buildIndicators(String nutrientType, int value) {
+    List<Widget> list = [];
+    for (int i = 0; i < 5; i++) {
+      list.add(
+        i < value
+            ? indicator(true, nutrientType)
+            : indicator(false, nutrientType),
+      );
+    }
+    return list;
+  }
+
+  Widget indicator(bool isActive, String nutrientType) {
+    IconData? icon;
+    switch (nutrientType) {
+      case 'Energy':
+        icon = FontAwesomeIcons.bolt;
+        break;
+      case 'Freshness':
+        icon = FontAwesomeIcons.glassWater;
+        break;
+      case 'Vitamin':
+        icon = Icons.rocket_launch_rounded;
+        break;
+      case 'Calories':
+        icon = FontAwesomeIcons.fire;
+        break;
+    }
+
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 10.0,
+        right: 5.0,
+      ),
+      child: Icon(
+        icon,
+        color: isActive == true ? product.nutrientsColor : Colors.black12,
+        size: 16,
       ),
     );
   }
