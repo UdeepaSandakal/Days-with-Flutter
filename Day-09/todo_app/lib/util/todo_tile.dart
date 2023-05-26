332e6e676e7b8fc362121c14020d64426686cdd4
UdeepaSandakal <2017asp02@vau.jfn.ac.lk>
2023-05-26 21:28:40 +0530

    line through effect

--- Day-09/todo_app/lib/util/todo_tile.dart
+++ Day-09/todo_app/lib/util/todo_tile.dart
@@ -15,23 +15,34 @@ class ToDoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
       child: Container(
         padding: const EdgeInsets.all(24.0),
+        decoration: BoxDecoration(
+          color: Colors.deepOrangeAccent,
+          borderRadius: BorderRadius.circular(12),
+        ),
         child: Row(
           children: [
             //chechbox
-            Checkbox(value: taskComplete, onChanged: onChanged),
+            Checkbox(
+              value: taskComplete,
+              onChanged: onChanged,
+              activeColor: Colors.black,
+            ),
 
             SizedBox(width: 20),
 
             // Task name
-            Text(taskName),
+            Text(
+              taskName,
+              style: TextStyle(
+                decoration: taskComplete
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
           ],
-        ),
-        decoration: BoxDecoration(
-          color: Colors.deepOrangeAccent,
-          borderRadius: BorderRadius.circular(12),
         ),
       ),
     );
