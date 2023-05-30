cf6afe67ffe8cc356814ded0d7a1d1cf804641a6
UdeepaSandakal <2017asp02@vau.jfn.ac.lk>
2023-05-30 22:39:54 +0530

    creating api key

--- Day-10/therma/lib/main.dart
+++ Day-10/therma/lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
-import 'package:therma/search_page.dart';
+import 'package:therma/data_service.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final _cityTextController = TextEditingController();
+  final _dataService = DataService();
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +18,87 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.grey[900],
-        body: SearchPage(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Container(
+                height: 300,
+                width: 300,
+                child: const FlareActor(
+                  "assets/WorldSpin.flr",
+                  fit: BoxFit.contain,
+                  animation: "roll",
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 32, right: 32),
+              child: Column(
+                children: <Widget>[
+                  const Text(
+                    "Search Weather",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white70),
+                  ),
+                  const Text(
+                    "Instanly",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w200,
+                        color: Colors.white70),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  TextFormField(
+                    controller: _cityTextController,
+                    decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.white70,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(
+                              color: Colors.white70, style: BorderStyle.solid)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          borderSide: BorderSide(
+                              color: Colors.blue, style: BorderStyle.solid)),
+                      hintText: "City Name",
+                      hintStyle: TextStyle(color: Colors.white70),
+                    ),
+                    style: const TextStyle(color: Colors.white70),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: MaterialButton(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      onPressed: _search,
+                      color: Colors.lightBlue,
+                      child: const Text(
+                        "Search",
+                        style: TextStyle(color: Colors.white70, fontSize: 16),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  void _search() {}
 }
