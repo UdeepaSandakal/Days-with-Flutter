364daf377959000f756fba2524aae991f27e2339
UdeepaSandakal <2017asp02@vau.jfn.ac.lk>
2023-05-30 13:45:36 +0530

    Creating WeatherModel class

---
+++ Day-10/therma/lib/show_weather.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShowWeather extends StatelessWidget {
+  WeatherModel weather;
+  final city;
+  ShowWeather(this.weather, this.city);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
